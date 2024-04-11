a4e7e8809be0bea21e503078aaf331c06f23bbe4
raymond <raymond.korir@student.moringaschool.com>
2024-04-11 19:15:43 +0300

    arrow-functions

---
+++ index.js
@@ -0,0 +1,11 @@
+// Function expression called divide
+const divide = function (num1, num2) {
+    return num1 / num2;
+  };
+  
+  // Arrow function called square
+  const square = (num) => num * num;
+  
+  // Arrow function called add
+  const add = (num1, num2) => num1 + num2;
+  
